9876c3288ca084dc5224523ee6b713cdc4fcc008
fddcn <4013465w@sina.com>
2016-02-24 16:21:41 +0800

    留言板优化，并支持换行

--- js/myjs.js
+++ js/myjs.js
@@ -1273,6 +1273,42 @@ function Getxiaoli() {
 		}
 	});
 }
+		getDateDiff = function(dateTimeStamp) {
+			//JavaScript函数：
+			var minute = 60;
+			var hour = minute * 60;
+			var day = hour * 24;
+			var halfamonth = day * 15;
+			var month = day * 30;
+			var year = day * 365;
+			var now = parseInt(new Date().getTime() / 1000);
+			var diffValue = now - dateTimeStamp;
+			if (diffValue < 0) {
+				//若日期不符则弹出窗口告之
+				//alert("结束日期不能小于开始日期！");
+			}
+			var yearC = diffValue / year;
+			var monthC = diffValue / month;
+			var weekC = diffValue / (7 * day);
+			var dayC = diffValue / day;
+			var hourC = diffValue / hour;
+			var minC = diffValue / minute;
+			if (yearC >= 1) {
+				result = "" + parseInt(monthC) + "年前";
+			} else if (monthC >= 1) {
+				result = "" + parseInt(monthC) + "个月前";
+			} else if (weekC >= 1) {
+				result = "" + parseInt(weekC) + "周前";
+			} else if (dayC >= 1) {
+				result = "" + parseInt(dayC) + "天前";
+			} else if (hourC >= 1) {
+				result = "" + parseInt(hourC) + "个小时前";
+			} else if (minC >= 1) {
+				result = "" + parseInt(minC) + "分钟前";
+			} else
+				result = "刚刚";
+			return result;
+		};
 /**
  *  对话框的HTML,很简单的布局!
   	<div class="layer_root">
