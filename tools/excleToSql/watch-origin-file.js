642c1d8a7889661386fe514da93d343248c7cc39
autumn <rememberwz@gmail.com>
2017-08-10 13:32:07 +0800

    tuisong

--- tools/excleToSql/watch-origin-file.js
+++ tools/excleToSql/watch-origin-file.js
@@ -1,6 +1,6 @@
 const http = require('http');
 let ydEtag = '';
-
+let key = require('./config.js').key
 var schedule = require('node-schedule');
 
 function scheduleCronstyle() {
@@ -9,7 +9,7 @@ function scheduleCronstyle() {
 		http.get('http://jwc.ytu.edu.cn/xk/bjkb.rar', (res, err) => {
 			if (res.headers.etag !== ydEtag && ydEtag != '') {
 				ydEtag = res.headers.etag;
-				http.get('http://sc.ftqq.com/KEY.send?text=' + encodeURIComponent('烟大课表更新啦~'));
+				http.get('http://pushbear.ftqq.com/sub?sendkey=' + key +'&text=' + encodeURIComponent('烟大课表更新啦~'));
 			}
         });
         
@@ -19,11 +19,11 @@ function scheduleCronstyle() {
             res.on('data', function(d) {
             body += d;
             });
-            var str = body.substr(0, 50)
             // WJ
             res.on('end', () => {
-                if (body.indexOf('http://jiaowu.wenjing.ytu.edu.cn/data/upload/ueditor/20170809/598ac3c6108c0.rar') <0) {
-                        http.get('http://sc.ftqq.com/KEY.send?text=' + encodeURIComponent('文经课表更新啦~') + '&desp=' + encodeURIComponent(str));
+                if (body.indexOf('http://jiaowu.wenjing.ytu.edu.cn/data/upload/ueditor/20170809/598ac3c6108c0.rar') <0 && body.length > 500) {
+                        var str = body.substr(0, 70)
+                        http.get('http://pushbear.ftqq.com/sub?sendkey=' + key +'&text=' + encodeURIComponent('文经课表更新啦~') + '&desp=' + encodeURIComponent(str));
                 }
             })
             // console.log(res.res)
